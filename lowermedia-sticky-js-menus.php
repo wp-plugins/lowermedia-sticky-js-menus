5bc1999ce4ef2f13344971fb11339b604861d542
hawkeye126 <hawkeye126@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-30 19:14:03 +0000

    tag version 3.1.0
    git-svn-id: https://plugins.svn.wordpress.org/lowermedia-sticky-js-menus/tags/3.1.0@1079170 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lowermedia-sticky-js-menus.php
+++ lowermedia-sticky-js-menus.php
@@ -1,164 +1,125 @@
 <?php
 /*
-Plugin Name: LowerMedia Sticky.js Menu's
+Plugin Name: LowerMedia Sticky.js Menus
 Plugin URI: http://lowermedia.net
 Description: WordPress plugin that integrates sticky.js and makes your primary navigation menu sticky (will 'stick' to top of screen when rolled over).  Activate and make your primary menu sticky!  Sticky means having your navigation always visible, the nav fixes itself to the top of the page.  This plugin uses the <a href='http://stickyjs.com'>Sticky.js</a> script, props and credit for creating that go to <a href="http://anthonygarand.com">Anthony Garand</a>, Thanks Anthony!   
-Version: 3.0.0
-Stable: 3.0.0
+Version: 3.1.0
+Stable: 4.1
 Author: Pete Lower
 Author URI: http://petelower.com
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-/*############################################################################################
-#	This plugin is designed to work out of the box with any theme by adding a class to 
-#	the menu container and then manipulating the HTML tag w/ said class by way of JS
-#	
-#	Plugins tested to work with this theme work a tad bit differently.  Instead of 
-#	adding a class it uses custom js files that have the main navigational selectors 
-#	already defined.  JS manipulates the menus by using the already defined tags. 
-#
-#
-#   This plugin has been tested on a growing number of themes including:
-#   
-#    twentytwelve, twentyeleven, responsive, wp-foundation, required-foundation, neuro, Swtor_NeozOne_Wp, 
-#    lowermedia_one_page_theme, expound, customizr, sixteen, destro, swift basic
-#
-*/
+/**
+ *	This plugin is designed to work out of the box with any theme by adding a class to 
+ *	the menu container and then manipulating the HTML tag w/ said class by way of JS
+ *	
+ *	Plugins tested to work with this theme work a tad bit differently.  Instead of 
+ *	adding a class it uses custom js files that have the main navigational selectors 
+ *	already defined.  JS manipulates the menus by using the already defined tags. 
+ *
+ *
+ *   This plugin has been tested on a growing number of themes including:
+ *   
+ *    twentytwelve, twentyeleven, responsive, wp-foundation, required-foundation, neuro, Swtor_NeozOne_Wp, 
+ *    lowermedia_one_page_theme, expound, customizr, sixteen, destro, swift basic
+ *
+ */
 
-/*############################################################################################
-#
-#   SECURITY: BLOCK DIRECT ACCESS TO FILE
-#
-*/
+/**
+ *
+ *   SECURITY: BLOCK DIRECT ACCESS TO FILE
+ *
+ */
 
-defined('ABSPATH') or die("Cannot access pages directly.");
+    defined('ABSPATH') or die("Cannot access pages directly.");
 
+/**
+ *   ENQUEUE AND LOCALIZE
+ *   Enqueue our 'iFrame On Demand' script and localize the plugin path for local asset use
+ *
+ */
 
-/*############################################################################################
-#
-#   ADD STICKY JS FILES/LIBRARIES(STICKY.JS)
-#   //This function adds sticky javascript libraries and files
-#
-*/
+if ( ! class_exists( 'LowerMedia_Sticky_JS_Menus' ) ) :
 
-	function lowermedia_add_sticky_js()  
-	{  
-		//collect info about the theme to point to theme specific js files
-		$theme_data = wp_get_theme();
+    class LowerMedia_Sticky_JS_Menus {
 
-		//collect option info from wp-admin/options.php
-		$lmstickyjs_options = get_option( 'lmstickyjs_option_name' );
+        const version = '3.1.0';
 
-		//Some themes have been defined specifically as to what the primary nav wrapper will be, 
-		//for the themes still in flux we'll add a class to the nav, this class is used in run-sticky.js
-		if ($theme_data['Template']!='twentytwelve' 
-			&& $theme_data['Template']!='twentyeleven' 
-			&& $theme_data['Template']!='twentyten' 
-			&& $theme_data['Template']!='wp-foundation' 
-			&& $theme_data['Template']!='required-foundation' 
-			&& $theme_data['Template']!='responsive' 
-			&& $theme_data['Template']!='neuro' 
-			&& $theme_data['Template']!='Swtor_NeozOne_Wp' 
-			&& $theme_data['Template']!='lowermedia_one_page_theme'
-			&& $theme_data['Template']!='expound'
-			&& $theme_data['Template']!='sixteen'
-			&& $theme_data['Template']!='destro'
-			&& $theme_data['Template']!='attitude'
-			&& $theme_data['Template']!='spun'
-			&& $theme_data['Template']!='Isabelle'
-			&& $theme_data['Template']!='spacious'
-			&& $theme_data['Template']!='bushwick'
-			&& $theme_data['Template']!='one-page')
-		{
-			function my_wp_nav_menu_args( $args = '' )
-				{
-					$args['container'] = 'nav';
-					$args['container_class'] = 'lowermedia_add_sticky';
-					return $args;
-				}
-				add_filter( 'wp_nav_menu_args', 'my_wp_nav_menu_args' );
+        function __construct() {
+
+            add_filter('the_content_more_link', array( __CLASS__, 'remove_more_jump_link' ));
+            add_action( 'wp_enqueue_scripts', array( __CLASS__, 'add_scripts' ) );
+
+        }
+
+        static function add_scripts() {
+   			wp_register_script( 'sticky', self::get_url( 'lowermedia.sticky.min.js' , __FILE__ ) , array( 'jquery' ), self::version, true);
+   			wp_localize_script( 'sticky', 'LMScriptParams', self::return_localization_information() );
+   			wp_enqueue_script( 'sticky' );
+        }
+
+        static function get_url( $path = '' ) {
+            return plugins_url( ltrim( $path, '/' ), __FILE__ );
+        }
+
+        static function nav_append_container_and_class( $args = '' ) {
+
+			$args['container'] = 'nav';
+			$args['container_class'] = 'lowermedia_add_sticky';
+			return $args;
+
 		}
 
-	    // Register and enque sticky.js - Sticky JS http://www.labs.anthonygarand.com/sticky - Anthony Garand anthonygarand.com
-		wp_register_script( 'sticky', plugins_url( '/js/jquery.sticky.js' , __FILE__ ) , array( 'jquery' ), '1.0.0', true);
-		wp_register_script( 'run-sticky', plugins_url( '/js/run-sticky.js' , __FILE__ ), array( 'sticky' ), '1.0.0', true);
-		wp_enqueue_script( 'run-sticky' );
+		static function return_localization_information() {
+			//collect option info from wp-admin/options.php
+			$lmstickyjs_options = get_option( 'lmstickyjs_option_name' );
+
+			$theme_info = wp_get_theme();
+
+			$params = array(
+			  'themename' => $theme_info['Template'],
+			  'stickytarget' => $lmstickyjs_options['lmstickyjs_class_selector'],
+			  'stickytargettwo' => $lmstickyjs_options['lmstickyjs_class_selector-two'],
+			  'disableatwidth' => $lmstickyjs_options['myfixed_disable_small_screen']
+			);
+
+			return $params;
+
+		}
+
+		static function remove_more_jump_link($link) { 
+	
+			$offset = strpos($link, '#more-');
 		
-		$params = array(
-		  'themename' => $theme_data['Template'],
-		  'stickytarget' => $lmstickyjs_options['lmstickyjs_class_selector'],
-		  'stickytargettwo' => $lmstickyjs_options['lmstickyjs_class_selector-two'],
-		  'disableatwidth' => $lmstickyjs_options['myfixed_disable_small_screen']
-		);
-
+			if ($offset) {
+				$end = strpos($link, '"',$offset);
+			}
 		
-		wp_localize_script( 'sticky', 'LMScriptParams', $params );
-		wp_localize_script( 'run-sticky', 'LMScriptParams', $params );
-	}  
-	add_action( 'wp_enqueue_scripts', 'lowermedia_add_sticky_js' ); 
+			if ($end) {
+				$link = substr_replace($link, '', $offset, $end-$offset);
+			}
 
-/*############################################################################################
-#
-#   ADD SETTINGS LINK UNDER PLUGIN NAME ON PLUGIN PAGE
-#   
-*/
-
-	add_filter('plugin_action_links', 'lowermedia_plugin_action_links', 10, 2);
-
-	function lowermedia_plugin_action_links($links, $file) {
-	    static $this_plugin;
-
-	    if (!$this_plugin) {
-	        $this_plugin = plugin_basename(__FILE__);
-	    }
-
-	    if ($file == $this_plugin) {
-	        // The "page" query string value must be equal to the slug
-	        // of the Settings admin page we defined earlier, which in
-	        // this case equals "myplugin-settings".
-	        $settings_link = '<a href="' . get_bloginfo('wpurl') . '/wp-admin/nav-menus.php">Set Menu</a>';
-	        array_unshift($links, $settings_link);
-	        $settings_link = '<a href="' . get_bloginfo('wpurl') . '/wp-admin/options-general.php?page=lm-stickyjs-settings">Settings</a>';
-	        array_unshift($links, $settings_link);
-	    }
-
-	    return $links;
-	}
-
-/*############################################################################################
-#
-#   Remove More Jump Link
-#   
-*/
-
-	function lmstickyjs_remove_more_jump_link($link) 
-	{ 
-	
-		$offset = strpos($link, '#more-');
-	
-		if ($offset) {
-			$end = strpos($link, '"',$offset);
+			return $link;
 		}
-	
-		if ($end) {
-			$link = substr_replace($link, '', $offset, $end-$offset);
-		}
-	
-		return $link;
-	}
-	
-	add_filter('the_content_more_link', 'lmstickyjs_remove_more_jump_link');
 
-/*############################################################################################
-#
-#   ADD ADMIN PAGE UNDER SETTINGS
-#   
-*/
+    }
 
-	class LowerMediaStickyAdminPage
-	{
+    if ( !is_admin() )
+    	$LowerMediaStickyJSMenus = new LowerMedia_Sticky_JS_Menus();
+
+endif;
+
+/**
+ *
+ *   ADD ADMIN PAGE UNDER SETTINGS
+ *   
+ */
+
+if ( ! class_exists( 'LowerMedia_Sticky_Admin_Page' ) ) :
+
+	class LowerMedia_Sticky_Admin_Page {
 	    //field callback values
 	    private $options;
 
@@ -167,7 +128,30 @@ defined('ABSPATH') or die("Cannot access pages directly.");
 	        add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
 	        add_action( 'admin_init', array( $this, 'page_init' ) );
 			add_action( 'admin_init', array( $this, 'lmstickyjs_default_options' ) );
+			add_filter('plugin_action_links_'.plugin_basename(__FILE__), array( $this , 'plugin_action_links' ), 10, 2);
 	    }
+
+	    static function plugin_action_links($links, $file) {
+
+		    static $this_plugin;
+
+		    if ( !$this_plugin ) {
+		        $this_plugin = plugin_basename(__FILE__);
+		    }
+
+		    if ( $file == $this_plugin ) {
+		        // The "page" query string value must be equal to the slug
+		        // of the Settings admin page we defined earlier, which in
+		        // this case equals "myplugin-settings".
+		        $settings_link = '<a href="' . get_bloginfo('wpurl') . '/wp-admin/nav-menus.php">Set Menu</a>';
+		        array_unshift($links, $settings_link);
+		        $settings_link = '<a href="' . get_bloginfo('wpurl') . '/wp-admin/options-general.php?page=lm-stickyjs-settings">Settings</a>';
+		        array_unshift($links, $settings_link);
+		    }
+
+		    return $links;
+
+		}
 
 	    //create options page
 	    public function add_plugin_page()
@@ -200,15 +184,8 @@ defined('ABSPATH') or die("Cannot access pages directly.");
 	            ?>
 	            <br/><br/>
 			        <center>
-			        I created this to save you time, for free :D It took some time, feel free to donate:<br/>
-			        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-					<input type="hidden" name="cmd" value="_s-xclick">
-					<input type="hidden" name="hosted_button_id" value="3S66QV3H7L49Y">
-					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-					<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-					</form>
-					</center>
-	            </form>
+			        	<a href="http://lowermedia.net/donate/">I created this to save you time, for free :D It took some time, feel free to donate</a>
+			        </center>
 	        </div>
 	        <?php
 	    }
@@ -325,6 +302,9 @@ defined('ABSPATH') or die("Cannot access pages directly.");
 		}
 		
 	}//END OF CLASS
-	if( is_admin() ) {$my_settings_page = new LowerMediaStickyAdminPage();}
-	// end plugin admin settings	
+
+	if( is_admin() )
+		$my_settings_page = new LowerMedia_Sticky_Admin_Page();
+
+endif;
 ?>
